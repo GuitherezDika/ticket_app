385750f8be39f62ffe657f8632ef6ed94755fb49
GuitherezDika <guitherez.git@gmail.com>
2024-10-03 06:49:02 +0700

    separate bottom nav file

--- lib/base/bottom_nav_bar.dart
+++ lib/base/bottom_nav_bar.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 
 class BottomNavBar extends StatefulWidget {
@@ -13,8 +14,33 @@ class _BottomNavBarState extends State<BottomNavBar> {
     // return const Placeholder();
     return Scaffold(
       appBar: AppBar(
-        
+        title: const Text('My Tickets'), 
       ),
-    )
+      body: const Center(
+        child: Text('Info Flutter'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.blueGrey,
+          unselectedItemColor: const Color(0xFF526400),
+          showSelectedLabels: false,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_home_add_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+                label: 'Search'),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+                label: 'Tickets'),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+                label: 'Profile'),
+          ]),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,18 @@
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/bottom_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(// seluruh screen hp
-      home: Scaffold( // scaffold = Inner screen
-        appBar: AppBar(
-          title: const Text('Book Tickets'),// bgColor, dst
-        ),
-        body: Center(
-          child: Text('Info Flutter'),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: Colors.blueGrey,
-          unselectedItemColor: const Color(0xFF526400),
-          showSelectedLabels: false,
-          items: [
-            BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_add_regular),
-              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled)
-            , label: 'Home'),
-            BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_search_filled),
-              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled)
-            , label: 'Search'),
-            BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
-              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_regular)
-            , label: 'Tickets'),
-            BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
-              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_regular)
-            , label: 'Profile'),
-            
-          ]
-          ),
-      )
+    return MaterialApp(
+      home: BottomNavBar(),
     );
-    
   }
 }
