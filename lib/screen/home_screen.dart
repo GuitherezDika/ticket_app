6ecad607812656a700e33b5d7ce2368db3d12e27
GuitherezDika <guitherez.git@gmail.com>
2024-10-04 09:47:13 +0700

    learn container-padding-edge insets

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -9,36 +9,43 @@ class HomeScreen extends StatelessWidget {
       // appBar: ,// untuk child screen tidak perlu set AppBar
       body: ListView(// Scrollable Effect
         children: [
-          Column(// box besar
-            children: [
-              Row(
-                // mainAxisAlignment = membuat komponen berderet
-                // berderet ke samping dan spacebetween
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // pemakaian Const pada widget = widget tersebut bernilai static 
-                  // akan ke render 1x / no rerender ketika widget lain ada update
-                  const Column(
-                    children: [
-                      Text('Good morning'),
-                      Text('Book Tickets'),
-                    ],
-                  ),
-                  Container(
-                    width: 100,
-                    height: 70,
-                    color: Colors.red,
-                  )
-                ],
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Search icon'),
-                  Text('Empty Space'),
-                ],
-              )
-            ],
+          Container(
+            color: Colors.amber,
+            // EdgeInsets dimasukkan dalam padding margin dll
+            // EdgeInsets = menentukan jarak tepi kanan, kiri, atas, bawah
+            // simetrik horizontal atau simetrik vertical
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(// box besar
+              children: [
+                Row(
+                  // mainAxisAlignment = membuat komponen berderet
+                  // berderet ke samping dan spacebetween
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // pemakaian Const pada widget = widget tersebut bernilai static 
+                    // akan ke render 1x / no rerender ketika widget lain ada update
+                    const Column(
+                      children: [
+                        Text('Good morning'),
+                        Text('Book Tickets'),
+                      ],
+                    ),
+                    Container(
+                      width: 100,
+                      height: 70,
+                      color: Colors.red,
+                    )
+                  ],
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Search icon'),
+                    Text('Empty Space'),
+                  ],
+                )
+              ],
+            ),
           )
         ],
       ),
