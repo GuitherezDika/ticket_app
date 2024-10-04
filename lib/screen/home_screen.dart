69ef85b843102d1765cc90f6d240c26edff5f89e
GuitherezDika <guitherez.git@gmail.com>
2024-10-04 09:23:57 +0700

    mainAxisAlignment spaceBetween

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,6 +5,44 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+    return Scaffold(
+      // appBar: ,// untuk child screen tidak perlu set AppBar
+      body: ListView(// Scrollable Effect
+        children: [
+          Column(// box besar
+            children: [
+              Row(
+                // mainAxisAlignment = membuat komponen berderet
+                // berderet ke samping dan spacebetween
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // pemakaian Const pada widget = widget tersebut bernilai static 
+                  // akan ke render 1x / no rerender ketika widget lain ada update
+                  const Column(
+                    children: [
+                      Text('Good morning'),
+                      Text('Book Tickets'),
+                    ],
+                  ),
+                  Container(
+                    width: 100,
+                    height: 70,
+                    color: Colors.red,
+                  )
+                ],
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Search icon'),
+                  Text('Empty Space'),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  } 
+
 }
